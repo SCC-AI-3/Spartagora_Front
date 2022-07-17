bbb4fd1d07e5eace28e06dd495329d57eb63c450
김혁진 <hyukjin720@gmail.com>
2022-07-18 06:18:40 +0900

    게시판 메인페이지 틀 만들기 #5

--- js/base.js
+++ js/base.js
@@ -1,8 +1,6 @@
-const toggleBtn = document.querySelector('.navbar_toogleBtn');
-const upper = document.querySelector('.navbar_upper');
-const icons = document.querySelector('.navbar_icons');
+const toggleBtn = document.querySelector('.Navbar_toogleBtn');
+const upper = document.querySelector('.Navbar_upper');
 
 toggleBtn.addEventListener('click', () => {
     upper.classList.toggle('active');
-    icons.classList.toggle('active');
 });
