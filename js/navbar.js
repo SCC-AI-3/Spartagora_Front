2bacdb9cd8a9bc22215f77b987700a305d4bfcaa
김혁진 <hyukjin720@gmail.com>
2022-07-20 10:30:45 +0900

    static, template 파일분리 / 게시판 부분 완성 #12

--- js/navbar.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.Navbar_toogleBtn');
-const upper = document.querySelector('.Navbar_upper');
-
-toggleBtn.addEventListener('click', () => {
-    upper.classList.toggle('active');
-});
