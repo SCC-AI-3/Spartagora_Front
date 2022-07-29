8b6ea76c88abd8a3de6790753eac4d6436a05146
Jeongin <lgj61111@naver.com>
2022-07-29 21:38:55 +0900

    글쓰기 기능 추가

--- templates/board_write/write_elements.js
+++ templates/board_write/write_elements.js
@@ -1,18 +1,31 @@
 const backend_base_url = "http://127.0.0.1:8000"
 const frontend_base_url = "http://127.0.0.1:5500"
 
-// 글 작성 기기
 async function postArticles() {
 
-    const ArticleData = {
-        category: document.getElementById('lowercategory').value,
-        title: document.getElementById('title').value,
-        summernote: document.getElementById('summernote').value,
-        tags: document.getElementById('tags').value
+    const title = document.getElementById('title').value
+    const summernote = document.getElementById('summernote').value
+    const category = document.getElementById('lowercategory').value
+    const tags = document.getElementById('tags').value
+
+    const formData = new FormData();
+    formData.append("title", title)
+    formData.append("content", summernote)
+    formData.append("lower_category", category)
+    formData.append("tags", tags)
+
+    const response = await fetch(`${backend_base_url}/article/`, {
+        headers: { "Authorization": "Bearer " + localStorage.getItem("access"), },
+        method: 'POST',
+        body: formData
+    }
+    )
+    if (response.status == 200) {
+        alert("업로드 완료!")
+        window.location.replace(`${frontend_base_url}/index.html`);
+    } else {
+        alert(response.status)
     }
 
-    // const response = await fetch()
-
-    console.log(ArticleData)
+    return response.json()
 }
-
