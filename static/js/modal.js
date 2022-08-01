bc0237146cb0a08ebf6c78c7efd3ed9328100e77
김혁진 <hyukjin720@gmail.com>
2022-08-01 17:59:14 +0900

    modify all link

--- static/js/modal.js
+++
@@ -1,6 +0,0 @@
-const writeModal = document.getElementById('writeModal')
-const writeInput = document.getElementById('writeModal')
-
-writeModal.addEventListener('shown.bs.Modal', function() {
-    writeInput.focus()
-})
