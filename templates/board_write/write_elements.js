fbf98b7f2510cb3f3a6e58300b16972d30a654ce
fattysphinxx <rlawjdgn159@gmail.com>
2022-08-02 01:20:55 +0900

    게시글 수정

--- templates/board_write/write_elements.js
+++ templates/board_write/write_elements.js
@@ -1,6 +1,8 @@
 const backend_base_url = "http://127.0.0.1:8000"
 const frontend_base_url = "http://127.0.0.1:5500"
 
+
+
 async function postArticles() {
     let random_emotion = ['미운', '고마운', '사랑하는', '증오하는', '끔찍한', '무서운']
     let random_nature = ['숲', '나무', '꽃', '풀', '바다', '절벽']
@@ -37,3 +39,62 @@ async function postArticles() {
 function goBack() {
     window.history.back();
 }
+
+
+// article edit page load
+async function loadArticleData() {
+    const article_id = location.href.split("?")[1]
+
+    const response = await fetch(`${backend_base_url}/article/detail/${article_id}/`, {
+        method: 'GET',
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("access")
+        },
+        withCredentials: true,
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.content)
+            document.getElementById("title").setAttribute('value', data.title)
+            document.getElementById("summernote").innerHTML = data.content
+            $("#summernote").summernote('code', data.content);
+            $('#summernote').summernote({
+                placeholder: '게시글을 작성해 주세요',
+                height: 300,
+                minHeight: null,
+                maxHeight: null,
+                focus: true
+            });
+        })
+}
+
+
+
+async function putArticle() {
+    const obj_id = location.href.split("?")[1]
+    const title = document.getElementById('title').value
+    const summernote = document.getElementById('summernote').value
+    const category = document.getElementById('lowercategory').value
+    const tags = document.getElementById('tags').value
+    const formData = new FormData();
+    formData.append("title", title)
+    formData.append("content", summernote)
+    formData.append("lower_category", category)
+    formData.append("tags", tags)
+
+    const response = await fetch(`${backend_base_url}/aticle/${obj_id}`, {
+        method: 'PUT',
+        headers: { Authorization: "Bearer " + localStorage.getItem("access"), },
+        body: formData
+    })
+    if (response.status == 200) {
+        window.location.replace(`${frontend_base_url}/templates/main.html/`);
+    } else {
+        alert("작성 실패")
+    }
+}
+
+$('document').ready(loadArticleData());
+
+
+
