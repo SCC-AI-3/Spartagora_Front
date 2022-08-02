e29069ba9a2cad74ad38bb45d0c13e9cdfc8435b
fattysphinxx <rlawjdgn159@gmail.com>
2022-08-03 02:22:09 +0900

    init, 경로수정

--- templates/recruit/api/api.js
+++ templates/recruit/api/api.js
@@ -7,7 +7,7 @@ async function logout() {
 }
 
 
-async function userarticleget() {
+async function recruitGet() {
     const category_id = location.href.split("?")[1]
     const categoryData = async () => {
         const response = await fetch(`${backend_base_url}/article/${category_id}/`, {
@@ -32,49 +32,10 @@ async function userarticleget() {
             let create_at = article[i]['created_at']
             let count = article[i]['count']
             let like = article[i]['like'].length
-            let like_id = article[i]['like']
             let comment_count = article[i]['comment_count']
             let lower_category_name = article[i]['lower_category_name']
             let nickname = article[i]['nickname']
-            const login_user = JSON.parse(localStorage.getItem("payload")).user_id
-            function contains(login_user) {
-                for (let i = 0; i < like_id.length; i++) {
-                    if (like_id[i] == login_user) {
-                        return true;
-                    }
-                }
-                return false;
-            }
-            if (contains(login_user) == true) {
-                let temp_html = `
-            <div class="List">
-                    <div class="ProfileInfo">
-                        <div class="Profile-Rtan">
-                            <img src="/image/0.png" alt="">
-                            <div class="User">
-                                <div>
-                                    <a href="#" class="Anonymous">${assignment}</a>
-                                    <span class="Anonymous">- ${nickname}</span>
-                                </div>
-                                <p class="UploadTime">${create_at}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="Article-Title">
-                        <a href="/templates/lower_category/qna.html?${id}" class="Title">${title}</a>
-                        <p class="Category">게시판 > ${lower_category_name}</p>
-                    </div>
-                    <div class="Count">
-                            <i class="fa-solid fa-thumbs-up"></i>${like}
-                            <i class="fa-regular fa-comments"></i>${comment_count}
-                            <i class="fa-solid fa-arrow-pointer"></i>${count}
-                    </div>
-                </div>
-            `
-                $('#article_list').append(temp_html)
-            }
-            else {
-                let temp_html = `
+            let temp_html = `
             <div class="List">
                     <div class="ProfileInfo">
                         <div class="Profile-Rtan">
@@ -99,8 +60,8 @@ async function userarticleget() {
                     </div>
                 </div>
             `
-                $('#article_list').append(temp_html)
-            }
+            $('#article_list').append(temp_html)
+
         }
     })
 }
@@ -108,4 +69,3 @@ async function userarticleget() {
 
 
 $('document').ready(userarticleget());
-
