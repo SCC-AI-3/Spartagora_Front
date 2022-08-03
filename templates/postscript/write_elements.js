2377cda4af747eca23f34ae8d5863f44db2154ba
fattysphinxx <rlawjdgn159@gmail.com>
2022-08-03 19:48:54 +0900

    오류 수정

--- templates/postscript/write_elements.js
+++ templates/postscript/write_elements.js
@@ -4,22 +4,21 @@ const frontend_base_url = "http://127.0.0.1:5500"
 
 
 async function postArticles() {
-
     let random_emotion = ['미운', '고마운', '사랑하는', '증오하는', '끔찍한', '무서운']
     let random_nature = ['숲', '나무', '꽃', '풀', '바다', '절벽']
     let random_name = random_emotion[Math.floor(Math.random() * random_emotion.length)] + " 나의 " + random_nature[Math.floor(Math.random() * random_nature.length)]
+    const title = document.getElementById('title').value
     const summernote = document.getElementById('summernote').value
     const category = document.getElementById('lowercategory').value
     const tags = document.getElementById('tags').value
     const nickname = random_name
-    const star = document.getElementById('article_star').value
 
     const formData = new FormData();
+    formData.append("title", title)
     formData.append("content", summernote)
     formData.append("lower_category", category)
     formData.append("tags", tags)
     formData.append("nickname", nickname)
-    formData.append("star", star)
 
     const response = await fetch(`${backend_base_url}/article/`, {
         headers: { "Authorization": "Bearer " + localStorage.getItem("access"), },
@@ -35,7 +34,6 @@ async function postArticles() {
     }
 
     return response.json()
-
 }
 
 function goBack() {
