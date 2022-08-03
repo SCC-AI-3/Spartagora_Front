dfe5c3c89ae30cd50644f2d5438615502c1b0e35
fattysphinxx <rlawjdgn159@gmail.com>
2022-08-03 18:10:54 +0900

    제목 필수로 안 받는 대신 게시판, 취업후기에 제목 꼭 받아야하는 조건 추가, 과정후기 별 받는 폼 생성

--- templates/postscript/write_elements.js
+++ templates/postscript/write_elements.js
@@ -4,43 +4,36 @@ const frontend_base_url = "http://127.0.0.1:5500"
 
 
 async function postArticles() {
-    if (out_result == "일반글") {
-        let random_emotion = ['미운', '고마운', '사랑하는', '증오하는', '끔찍한', '무서운']
-        let random_nature = ['숲', '나무', '꽃', '풀', '바다', '절벽']
-        let random_name = random_emotion[Math.floor(Math.random() * random_emotion.length)] + " 나의 " + random_nature[Math.floor(Math.random() * random_nature.length)]
-        const title = document.getElementById('title').value
-        const summernote = document.getElementById('summernote').value
-        const category = document.getElementById('lowercategory').value
-        const tags = document.getElementById('tags').value
-        const nickname = random_name
+    let random_emotion = ['미운', '고마운', '사랑하는', '증오하는', '끔찍한', '무서운']
+    let random_nature = ['숲', '나무', '꽃', '풀', '바다', '절벽']
+    let random_name = random_emotion[Math.floor(Math.random() * random_emotion.length)] + " 나의 " + random_nature[Math.floor(Math.random() * random_nature.length)]
+    const summernote = document.getElementById('summernote').value
+    const category = document.getElementById('lowercategory').value
+    const tags = document.getElementById('tags').value
+    const nickname = random_name
+    const star = document.getElementById('article_star').value
 
-        const formData = new FormData();
-        formData.append("title", title)
-        formData.append("content", summernote)
-        formData.append("lower_category", category)
-        formData.append("tags", tags)
-        formData.append("nickname", nickname)
+    const formData = new FormData();
+    formData.append("content", summernote)
+    formData.append("lower_category", category)
+    formData.append("tags", tags)
+    formData.append("nickname", nickname)
+    formData.append("star", star)
 
-        const response = await fetch(`${backend_base_url}/article/`, {
-                headers: {"Authorization": "Bearer " + localStorage.getItem("access"),},
-                method: 'POST',
-                body: formData
-            }
-        )
-        if (response.status == 200) {
-            alert("업로드 완료!")
-            window.location.replace(`${frontend_base_url}/templates/mainpage/main.html`);
-        } else {
-            alert(response.status)
-        }
-
-        return response.json()
+    const response = await fetch(`${backend_base_url}/article/`, {
+        headers: { "Authorization": "Bearer " + localStorage.getItem("access"), },
+        method: 'POST',
+        body: formData
+    }
+    )
+    if (response.status == 200) {
+        alert("업로드 완료!")
+        window.location.replace(`${frontend_base_url}/templates/postscript/postscript_main.html`);
+    } else {
+        alert(response.status)
     }
 
-    else {
-        alert('공격발언 및 차별발언 입니다!')
-    }
-
+    return response.json()
 }
 
 function goBack() {
@@ -79,34 +72,30 @@ async function loadArticleData() {
 
 
 async function putArticle() {
-    if (out_result == "일반글") {
-        const obj_id = location.href.split("?")[1]
-        const title = document.getElementById('title').value
-        const summernote = document.getElementById('summernote').value
-        const category = document.getElementById('lowercategory').value
-        const tags = document.getElementById('tags').value
-        const formData = new FormData();
-        formData.append("title", title)
-        formData.append("content", summernote)
-        formData.append("lower_category", category)
-        formData.append("tags", tags)
+    const obj_id = location.href.split("?")[1]
+    const title = document.getElementById('title').value
+    const summernote = document.getElementById('summernote').value
+    const category = document.getElementById('lowercategory').value
+    const tags = document.getElementById('tags').value
+    const formData = new FormData();
+    formData.append("title", title)
+    formData.append("content", summernote)
+    formData.append("lower_category", category)
+    formData.append("tags", tags)
 
-        const response = await fetch(`${backend_base_url}/article/put/${obj_id}/`, {
-            method: 'PUT',
-            headers: {Authorization: "Bearer " + localStorage.getItem("access"),},
-            body: formData
-        })
-        if (response.status == 200) {
-            window.location.replace(`${frontend_base_url}/templates/mainpage/main.html`);
-        } else {
-            alert("작성 실패")
-        }
-    }
-
-    else {
-        alert('공격발언 및 차별발언 입니다!')
+    const response = await fetch(`${backend_base_url}/article/put/${obj_id}/`, {
+        method: 'PUT',
+        headers: { Authorization: "Bearer " + localStorage.getItem("access"), },
+        body: formData
+    })
+    if (response.status == 200) {
+        window.location.replace(`${frontend_base_url}/templates/postscript/postscript_main.html`);
+    } else {
+        alert("작성 실패")
     }
 }
+
+
 $('document').ready(loadArticleData());
 
 
