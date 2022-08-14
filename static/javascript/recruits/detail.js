c7ef6f52d77af18dd9f07b25724ac0d28852b047
김혁진 <hyukjin720@gmail.com>
2022-08-15 06:53:22 +0900

    modify base url

--- static/javascript/recruits/detail.js
+++ static/javascript/recruits/detail.js
@@ -1,5 +1,5 @@
-const backend_base_url = "http://127.0.0.1:8000"
-const frontend_base_url = "http://127.0.0.1:5500"
+const backend_base_url = "https://13.124.20.151"
+const frontend_base_url = "https://d2b1lm00cp7z1g.cloudfront.net/"
 // article edit button click
 function editButtonClick() {
     let edit_link = window.location.search.split('?')[1]
